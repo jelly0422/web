9868360ee2501e6aa52afe162ebf046b9e205bae
陈国栋 <1187893119@qq.com>
2021-01-01 03:11:33 +0800

    商品模块

---
+++ src/components/GoodsModule.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>{{ module }}</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoodsModule",
+  data(){
+
+  },
+  props: {
+    module:{
+      type: String,
+      required: true
+    }
+  },
+  methods:{
+
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
