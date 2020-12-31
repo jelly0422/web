30dcaafc061ec8ddf160a063d57fba8ad96d0cd5
陈国栋 <1187893119@qq.com>
2020-12-31 18:36:44 +0800

    路由配置

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomePage from "../components/HomePage";
+import Login from "../components/Login";
+import Regist from "../components/Regist";
+import MainStore from "../components/MainStore";
+import GoodsInfo from "../components/GoodsInfo";
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+      children:[
+        { path:'/', component: MainStore},
+        { path:'goodsinfo', component: GoodsInfo},]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/regist',
+      name: 'Regist',
+      component: Regist
+    },
+
+  ]
+})
