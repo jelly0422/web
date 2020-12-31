6919b2f4bf71e16fb1504879d3486a96083f4ffc
陈国栋 <1187893119@qq.com>
2020-12-31 18:34:47 +0800

    根组件

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 id="title" align="center">FashionMall</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+#title{
+  font-family: FBLawnmower,serif;
+  font-size: 75px;
+}
+</style>
