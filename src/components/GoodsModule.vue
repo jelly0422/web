9d631ed3d258a1bffa61edb4b8a0f6052aba7062
陈国栋 <1187893119@qq.com>
2021-01-05 22:49:44 +0800

    商品模块组件

--- src/components/GoodsModule.vue
+++ src/components/GoodsModule.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <el-container>
-      <el-header>{{ module }}</el-header>
-      <el-main>Main</el-main>
+      <el-header style="font-size: 18px">{{ module }}</el-header>
+      <el-main>
+        <div id="goods" style="position: absolute"></div>
+        <Goods style="margin-left: 250px"></Goods>
+      </el-main>
     </el-container>
   </div>
 </template>
 
 <script>
+import Goods from "./Goods";
 export default {
   name: "GoodsModule",
+  components:{
+    Goods
+  },
   data(){
     return{}
   },
@@ -29,5 +36,9 @@ export default {
 </script>
 
 <style scoped>
-
+#goods{
+  width: 225px;
+  height: 300px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+}
 </style>
