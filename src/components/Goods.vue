a650b5882f18fddcc580f481b479b0075b6a3516
陈国栋 <1187893119@qq.com>
2021-01-05 22:49:34 +0800

    商品组件

--- src/components/Goods.vue
+++ src/components/Goods.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="goods">
     <div id="pic">
-      <a href="">
+      <span @click="jump">
         <el-image
           style="width: 140px; height: 175px"
           :src="'http://8.129.71.20/1.jpg'"
           :fit="'fill'"></el-image>
-      </a>
+      </span>
     </div>
-    <span><a href="">
+    <span><a href="#/goodinfo" @click="jump">
       <div id="other">
         <div class="info">商品名称</div>
         <br>
@@ -21,7 +21,27 @@
 <script>
 export default {
   name: "Goods",
-
+  data(){
+    return{
+      goods:{name: "大衣",price: 98,info: "lkahslebn"}
+    }
+  },
+  methods:{
+    jump:function () {
+      this.$router.push({
+        //path后面跟跳转的路由地址
+        path: '/goodsinfo',
+        //name后面跟跳转的路由名字（必须有亲测，不使用命名路由会传参失败）
+        name: 'GoodsInfo',
+        params: {
+          //imgsListsUrl2是自己定义的名字，this.imgsListsUrl是要被传递的值
+          name: this.goods.name,
+          price: this.goods.price,
+          info: this.goods.info
+        }
+      })
+    }
+  }
 }
 </script>
 
