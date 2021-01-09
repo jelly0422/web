83f0e9d060cd8dfb15279f3c2136c96a458faf92
陈国栋 <1187893119@qq.com>
2021-01-10 02:50:57 +0800

    路由表

--- src/router/index.js
+++ src/router/index.js
@@ -15,19 +15,31 @@ import UserCtrl from "../components/UserCtrl";
 import UserInfo from "../components/UserInfo";
 import UserAddress from "../components/UserAddress";
 import Order from "../components/Order";
+import ShowGoods from "../components/ShowGoods";
 
 Vue.use(Router)
+
+//获取原型对象上的push函数
+const originalPush = Router.prototype.push
+//修改原型对象中的push方法
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/',name: 'homepage',
       component: HomePage,redirect: '/mainstore',
       children: [
         {path: 'mainstore', component: MainStore},
-        {path: 'goodsinfo', name:GoodsInfo, component: GoodsInfo},
+        {path: 'goodsinfo', name:'GoodsInfo', component: GoodsInfo},
         {path: 'goods', component: Goods},
         {path: 'cart', component: Cart},
+        {
+          path: 'showgoods', name: 'ShowGoods', component: ShowGoods,
+
+        },
         {
           path: 'userctrl',
           component: UserCtrl,
