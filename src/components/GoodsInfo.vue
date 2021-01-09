9b3dad0fcd4bb93be07ad75e2b486d4350b4f5a2
陈国栋 <1187893119@qq.com>
2021-01-10 02:50:15 +0800

    商品详细信息组件

--- src/components/GoodsInfo.vue
+++ src/components/GoodsInfo.vue
@@ -3,29 +3,42 @@
   <div>
     <div id="imgs">
       <el-carousel indicator-position="outside" height="450px">
-        <el-carousel-item v-for="item in imgList" :key="item">
+        <el-carousel-item v-for="(item,index) in imgList" :key="index">
           <div><img v-bind:src="item.url" alt=""></div>
         </el-carousel-item>
       </el-carousel>
     </div>
     <form action="#" class="goodsinfo">
       <div>
-        <span><p id="name">商品名称</p></span>
+        <span><p id="name">{{Goods.name}}</p></span>
         <el-divider></el-divider>
-        <div><span><p>商品信息</p></span></div>
-        <span><p>价格</p></span>
+        <div><span><p>{{ Goods.introduce }}</p></span></div>
+        <br>
+        <span><p style="color: coral">￥{{ Goods.money }}</p></span>
 
         <div>
           <el-divider>选择商品尺寸</el-divider>
           <el-radio-group v-model="radio1">
-            <el-radio label="1" border>大</el-radio>
-            <el-radio label="2" border>中</el-radio>
-            <el-radio label="3" border>小</el-radio>
+            <el-radio label="2" border>大</el-radio>
+            <el-radio label="1" border>中</el-radio>
+            <el-radio label="0" border>小</el-radio>
           </el-radio-group>
         </div>
-        <el-divider></el-divider>
-        <el-button type="primary" round icon="el-icon-shopping-cart-2">加入购物车</el-button>
+        <el-divider>选择数量</el-divider>
+        <div><el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number></div>
+        <br>
+        <el-button type="primary" round icon="el-icon-shopping-cart-2" @click="addToCart">加入购物车</el-button>
       </div>
+      <el-dialog
+        title="提示"
+        :visible.sync="dialogVisible"
+        width="30%"
+        >
+        <span>请登陆后将商品添加到购物车</span>
+        <span slot="footer" class="dialog-footer">
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+      </el-dialog>
     </form>
     <br>
     <br>
@@ -35,25 +48,64 @@
 </template>
 
 <script>
+import Global from "./global/Global";
 export default {
   name: "GoodsInfo",
   data (){
     return {
-      Goods:[],
-      imgList:[
-        {url:'http://8.129.71.20/1.jpg'}
-      ],
+      Goods: {},
+      imgList:[],
       radio1: '1',
+      dialogVisible: false,
+      num: 1
     }
   },
   methods:{
-
+    addToCart: function () {
+      if (Global.isLogin.isLogin===false){
+        this.dialogVisible = true
+      }else{
+        this.$http("/addClothes",{params:{userid:localStorage.getItem("id"),
+            clothesid:this.Goods.id,
+            money: this.Goods.money * this.num,
+            number: this.num,
+            size: this.radio1}}).then(res=>{
+          if (res.data === 1){
+            this.$notify({
+              title: '成功',
+              message: '添加成功',
+              type: 'success'
+            });
+          }else{
+            this.$notify.error({
+              title: '错误',
+              message: '添加失败'
+            });
+          }
+        })
+      }
+    },
+    //测试计数值用
+    handleChange(value) {
+      console.log(value);
+      console.log(this.num);
+    }
   },
   created() {
-    this.$http.get('url').then(res=>{
-      this.Goods = res.data
-    })
-  }
+  },
+  mounted () {
+    //this.imgsListsUrl2是自己在B组件里定义的数据
+    //this.$route.params.imgsListsUrl2是传过来的参数
+    if (this.$route.params.goods === undefined){
+      this.Goods = this.$route.query.goods;
+      //console.log(this.Goods);
+    }else{
+      this.Goods = this.$route.params.goods;
+    }
+    this.imgList = [{url:'http://8.129.71.20/photo/' + this.Goods.p2 + '.jpg'},
+                    {url:'http://8.129.71.20/photo/' + this.Goods.p3 + '.jpg'},
+                    {url:'http://8.129.71.20/photo/' + this.Goods.p4 + '.jpg'}]
+  },
 }
 </script>
 
