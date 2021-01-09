bccc81c5c0daf33d92dc37447db1f16e2f962872
陈国栋 <1187893119@qq.com>
2021-01-10 02:52:26 +0800

    订单组件

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -4,21 +4,51 @@
       <div slot="header" class="clearfix">
         <span style="font-size: 22px">用户订单</span>
       </div>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{'列表内容 ' + o }}
+      <div v-for="(item,index) in orderList" :key="index" class="text item">
+        <div style="font-size: 20px">{{item.name}}</div>
+        <div style="color: coral; float: right">总价：{{item.money}}</div>
+        <br>
+        <div>
+          <span>数量：{{item.number}}</span>
+          <span style="margin-left: 10%">尺寸：{{size[item.size]}}</span>
+        </div>
+        <div style="float: right">下单时间：{{item.time.split('.')[0].replace('T',' ')}}</div>
         <div><el-divider></el-divider></div>
       </div>
+      <span ref="noOrder" style="visibility: visible;font-size: 18px">暂无订单信息</span>
     </el-card>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: "Order",
   data (){
     return{
-      orderList:[]
+      orderList:[],
+      size: ["小","中","大"]
     }
+  },
+  methods:{
+    doCreate(){
+      this.$http('getOrder',{params:{userid: localStorage.getItem("id")}}).then(res=>{
+        console.log(res);
+        this.orderList = res.data
+        if (res.data.length !==0){
+          this.$refs.noOrder.style.visibility = "hidden"
+        }
+      })
+    },
+    trans(val){
+      val.replace("T"," ")
+      val.split('.')
+      console.log(val);
+    }
+  },
+  created() {
+    this.doCreate()
   }
 }
 </script>
